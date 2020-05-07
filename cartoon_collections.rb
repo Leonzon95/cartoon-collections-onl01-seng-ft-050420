179b61f1d269bbdc2b4bc49b9dba9bbdeb0ff671
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-07 00:27:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.detect do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
