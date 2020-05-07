8038c4fd78e4ebaa56aab55539e27ab753be9419
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-07 00:16:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,12 @@
 def roll_call_dwarves(array)
   array.each_with_index do |name, i|
-    puts "#{i + 1 name}"
+    puts "#{i + 1} #{name}"
   end
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
-end
+end  
 
 def long_planeteer_calls# code an argument here
   # Your code here
